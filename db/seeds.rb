5ead18df2a1564bb625c287029814bb4c1b0fcc7
Shakti Patel <shaktip258@gmail.com>
2016-10-09 03:23:56 +0000

    Edit movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,3 @@ more_movies = [
   {:title => 'Raiders of the Lost Ark', :rating => 'PG',
     :release_date => '12-Jun-1981'}
 ]
-# NOTE: the following line temporarily allows mass assignment
-# (needed if you used attr_accessible/attr_protected in movie.rb)
-Movie.send(:attr_accessible, :title, :rating, :release_date)
-more_movies.each do |movie|
-  Movie.create!(movie)
-end
