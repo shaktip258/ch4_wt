fe1dbb885403a24b25314a017c6d98d169f15401
Shakti Patel <shaktip258@gmail.com>
2016-10-09 04:48:51 +0000

    Add edit view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,13 +22,13 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find params[:id]
   end
-  
+
   def update
     @movie = Movie.find params[:id]
     @movie.update_attributes!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
-  end
+end
   
   def destroy
     @movie = Movie.find(params[:id])
