e328974050039559960763b0c2c0565a515a4357
Shakti Patel <shaktip258@gmail.com>
2016-10-09 04:19:45 +0000

    Edit movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,4 +18,17 @@ class MoviesController < ApplicationController
   flash[:notice] = "#{@movie.title} was successfully created."
   redirect_to movies_path
   end
+  
+  # in movies_controller.rb
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+  
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
 end
